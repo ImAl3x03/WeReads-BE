d84a1c79856f28f8318f2140fc993f8ca2159358
Alex Di Maria <alessandro@sophany.it>
2022-07-14 23:49:47 +0200

    feat: connectio to mongoDB DataBase

---
+++ src/db/database.ts
@@ -0,0 +1,28 @@
+import { config } from 'dotenv';
+import { MongoClient } from 'mongodb';
+import Log from '../log/log';
+
+const log = new Log();
+config();
+
+const dbConnectionString: string = process.env.DATABASE_CONNECTION_STRING ?? "";
+const dbName: string = process.env.DATABASE ?? "";
+const collectionName: string = process.env.COLLECTION ?? "";
+
+const client = new MongoClient(dbConnectionString);
+
+const connect = async () => {
+    try {
+        await client.connect();
+        log.info("Connecting to database...")
+    } catch(err: any) {
+        log.error(`Database connection not provided \n${err.message}`)
+    }
+}
+
+connect();
+let db = client.db(dbName);
+let collection = db.collection(collectionName)
+
+export default db;
+export {collection};
